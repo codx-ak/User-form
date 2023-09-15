c3a88c09263cfae5caf8af8f1b3053db35bb3303
Ak <moorthimt15@gmail.com>
2023-09-15 21:15:11 +0530

    loader adding

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Container} from '@mui/material'
-import './spinner.css'
+
 const Loader = () => {
   return (
     <Container className='spinner'>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,13 +1,12 @@
-import { Container } from '@mui/material'
 import React from 'react'
 import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
-    <Container className="page-404">
+    <div className="page-404">
     <h1>404</h1>
     <p>Page Not Found !!!</p>
     <Link to="/">Go Back</Link>
-    </Container>
+    </div>
   )
 }
 
